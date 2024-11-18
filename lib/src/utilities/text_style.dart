7384e7cd617d6d14694e330889fd06429143ac3c
DevadathAR <devadathar001@gmail.com>
2024-11-18 13:16:10 +0530

    add project strature

---
+++ lib/src/utilities/text_style.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyle {
+
+  static TextStyle regularText(
+          {required double size, Color? color, FontStyle? fontstyle}) =>
+      TextStyle(
+          fontSize: size,
+          fontFamily: "Nunito_Sans",
+          fontWeight: FontWeight.w400,
+          color: color,
+          fontStyle: fontstyle);
+  static TextStyle mediumText({required double size, Color? color}) =>
+      TextStyle(
+        fontSize: size,
+        fontFamily: "Nunito_Sans",
+        fontWeight: FontWeight.w500,
+        color: color,
+      );
+  static TextStyle semiboldText({required double size, Color? color}) =>
+      TextStyle(
+        fontSize: size,
+        fontFamily: "Nunito_Sans",
+        fontWeight: FontWeight.w600,
+        color: color,
+      );
+  static TextStyle boldText({required double size, Color? color}) => TextStyle(
+        fontSize: size,
+        fontFamily: "Nunito_Sans",
+        fontWeight: FontWeight.w700,
+        color: color,
+      );
+}
