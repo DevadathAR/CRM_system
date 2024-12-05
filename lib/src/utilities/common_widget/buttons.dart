187c65ad3fb3544980d9ffb8bc62f13e6a085348
DevadathAR <devadathar001@gmail.com>
2024-12-05 17:52:39 +0530

    mereged , edit button widget

--- lib/src/utilities/common_widget/buttons.dart
+++ lib/src/utilities/common_widget/buttons.dart
@@ -4,62 +4,62 @@ import 'package:crm_system/src/utilities/text_style.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
-class Buttons extends StatelessWidget {
-  final double? length;
-  final Color? color;
-  final String label;
-  final bool back;
-  final bool forward;
+// class Buttons extends StatelessWidget {
+//   final double? length;
+//   final Color? color;
+//   final String label;
+//   final bool back;
+//   final bool forward;
 
-  final VoidCallback? ontap;
+//   final VoidCallback? ontap;
 
-  final Color? textColor;
+//   final Color? textColor;
 
-  const Buttons({
-    super.key,
-    this.length = 280,
-    required this.label,
-    this.color,
-    this.textColor,
-    required this.ontap,
-    this.back = false,
-    this.forward = false,
-  });
+//   const Buttons({
+//     super.key,
+//     this.length = 280,
+//     required this.label,
+//     this.color,
+//     this.textColor,
+//     required this.ontap,
+//     this.back = false,
+//     this.forward = false,
+//   });
 
-  @override
-  Widget build(BuildContext context) {
-    return InkWell(
-      onTap: ontap,
-      child: Container(
-        height: 50,
-        width: length,
-        decoration: BoxDecoration(
-          boxShadow: [
-            BoxShadow(
-                color: AppColors.textGrey1,
-                blurRadius: 12,
-                offset: const Offset(0, 6))
-          ],
-          borderRadius: const BorderRadius.all(Radius.circular(14)),
-          color: color ?? AppColors.blue,
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            if (back) SvgPicture.asset(arrowBackSvg),
-            if (back) const SizedBox(width: 8),
-            Text(
-              label,
-              style: AppTextStyle.boldText(size: 16, color: textColor),
-            ),
-            if (forward) const SizedBox(width: 8),
-            if (forward) SvgPicture.asset(arrowForwardSvg),
-          ],
-        ),
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return InkWell(
+//       onTap: ontap,
+//       child: Container(
+//         height: 50,
+//         width: length,
+//         decoration: BoxDecoration(
+//           boxShadow: [
+//             BoxShadow(
+//                 color: AppColors.textGrey1,
+//                 blurRadius: 12,
+//                 offset: const Offset(0, 6))
+//           ],
+//           borderRadius: const BorderRadius.all(Radius.circular(14)),
+//           color: color ?? AppColors.blue,
+//         ),
+//         child: Row(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             if (back) SvgPicture.asset(arrowBackSvg),
+//             if (back) const SizedBox(width: 8),
+//             Text(
+//               label,
+//               style: AppTextStyle.boldText(size: 16, color: textColor),
+//             ),
+//             if (forward) const SizedBox(width: 8),
+//             if (forward) SvgPicture.asset(arrowForwardSvg),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
 
 class RadioButtonRow extends StatefulWidget {
   final List<String> options; // List of radio button options
